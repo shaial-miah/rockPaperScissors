e3781e4f47d7d547032b8af595f33044d365723d
Shaial Miah <shaial@hotmail.co.uk>
2025-12-03 22:34:19 +0000

    add ability for player to select either rock, paper or scissors by clicking the button. Also had the robots random selection of rock, paper or scissors

--- game Robot/gameRobot.js
+++ game Robot/gameRobot.js
@@ -11,18 +11,22 @@ const scissorIcon = document.getElementById("scissorIcon");
 // first put all icons into an array
 const icons = [rockIcon, paperIcon, scissorIcon];
 
+let userChoice;
+
 const selectOneIconOnly = (icon) => {
 
     for (let i = 0; i < icons.length; i++) {
         icons[i].classList.remove("selected");
     };
-    
-    icon.classList.toggle("selected");
+
+    icon.classList.add("selected");
 };
 
 selectRockButton.addEventListener("click", () => {
 
     selectOneIconOnly(rockIcon);
+
+    userChoice = 'rock';
 
 });
 
@@ -30,10 +34,42 @@ selectPaperButton.addEventListener("click", () => {
 
     selectOneIconOnly(paperIcon);
 
+    userChoice = 'paper';
+
 });
 
 selectScissorButton.addEventListener("click", () => {
 
     selectOneIconOnly(scissorIcon);
 
+    userChoice = 'scissors';
+
 });
+
+// Random robot selection
+
+// This will assign a number between 0 and 3 excluding 3.
+// These numbers will be our index numbers for our icons array.
+const randomNumber = Math.floor(Math.random() * 3);
+const selections = ['rock', 'paper', 'scissors'];
+
+
+const randomSelection = () => {
+
+    if (selections[randomNumber] === 'rock') {
+        rockIcon.classList.add("selectedRobot");
+    }
+
+    if (selections[randomNumber] === 'paper') {
+        paperIcon.classList.add("selectedRobot");
+    }
+
+    if (selections[randomNumber] === 'scissors') {
+        scissorIcon.classList.add("selectedRobot");
+    }
+
+};
+
+setTimeout(() => {
+    randomSelection();
+}, 3000);
