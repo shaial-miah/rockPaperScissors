c030093bed7d9b4b927f5b73f5093473856ab8e3
Shaial Miah <shaial@hotmail.co.uk>
2025-12-07 14:51:56 +0000

    commit all files to repo

--- index/script.js
+++ index/script.js
@@ -1,21 +1,41 @@
 let playButton = document.getElementById("playButton");
 let inputField = document.getElementById("inputName");
 
+const createNewPlayer = () => {
+
+     // obtain input value from input field.
+     let inputName = inputField.value.trim();
+     localStorage.setItem("playerName", inputName);
+     
+     let tableRow = document.createElement("tr");
+
+     let tableData = document.createElement("td");
+
+     tableData.innerHTML = inputName;
+
+     let span = document.createElement("span");
+     
+     span.classList.add("points");
+
+     span.innerText = "0";
+
+     // construct the element
+
+     tableRow.appendChild(tableData);
+
+     tableRow.appendChild(span);
+
+     document.getElementById("leaderboardTable").appendChild(tableRow);
+
+};
+
 playButton.addEventListener("click", ()=> {
 
     if (inputField.value === "") {
         window.alert("Enter your name to proceed: ")
     } else {
 
-        // obtain input value from input field.
-        let inputName = inputField.value.trim();
-        localStorage.setItem("playerName", inputName);
-        
-        let tableRow = document.createElement("tr");
-
-        tableRow.innerHTML = `<td> ${inputName} </td> <td> 0 points </td>`;
-
-        document.getElementById("leaderboardTable").appendChild(tableRow);
+        createNewPlayer();
 
        //  window.location.href = "/game Modes/gameMode.html";
     }
